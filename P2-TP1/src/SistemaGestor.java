5d9a768ba4e7e6d2f1b60c97e84447f600df35f1
paicodev <paedmaag@gmail.com>
2026-04-08 19:55:39 -0300

    init: Se creó el proyecto y el primer ejercicio
    -Se realizó el primer ejercicio con lo solicitado (comentarios de tres tipos // /* y /** */.

---
+++ P2-TP1/src/SistemaGestor.java
@@ -0,0 +1,10 @@
+/** 
+ *   Esta clase se encarga de gestionar el sistema 
+ * */
+public class SistemaGestor{
+// Atributos de ejemplo
+                              
+/* 
+   Bloque de configuracion de ejemplo 
+*/
+}
